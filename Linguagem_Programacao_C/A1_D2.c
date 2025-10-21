bdb83fe6ca9def7b58105c7e4daab3919b5451e3
Billy (wcabilly) <wcabilly@gmail.com>
2025-10-21 12:49:24 -0300

    A1. Linguagem de Programação - D2

---
+++ Linguagem_Programacao_C/A1_D2.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main (){
+    int a, b, c;
+    a = 10;
+    b = 20;
+    c = a + b; // Calculate the sum of a and b
+    printf("A soma de %d e %d é %d\n", a, b, c); // Print the result
+    return 0;
+} 
